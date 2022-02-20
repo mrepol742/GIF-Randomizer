3eb64e4f88e20ccd3e782b5bacee41e270aae664
mrepol742 <mrepol742@gmail.com>
2022-02-20 10:24:22 +0800

    Initial Commit

--- api/index.php
+++ api/index.php
@@ -27,10 +27,6 @@ $gif = array("https://tenor.com/view/detective-detective-conan-anime-eyeglasses-
     "https://tenor.com/view/detective-conan-conan-edogawa-gremlin-smile-mischevious-gif-19597821.gif",
     "https://tenor.com/view/detective-conan-glasses-gif-23905397.gif");
 $pk = $gif[mt_rand(1, count($gif))];
-echo $pk;
-//header("Location: " . $pk);
-echo '<script type="text/javascript">
-           window.location = "' . $pk . '"
-      </script>';
+header("Location: " . $pk);
 die();
 ?>
